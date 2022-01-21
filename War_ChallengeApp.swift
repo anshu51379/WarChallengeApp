0e9bdb2ba0910397e0cd89c2728573131b07c8c1
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2022-01-21 19:48:11 +0530

    Add files via upload

---
+++ War_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_ChallengeApp.swift
+//  War-Challenge
+//
+//  Created by Abhishek Kumar on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct War_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
